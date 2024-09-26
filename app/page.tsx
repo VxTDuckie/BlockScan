f11e32622eec7db0d2c7848169f0371e5e3808a8
Levironexe <nguyenphuoc4805@gmail.com>
2024-09-26 16:02:13 +0700

    BlockScan project

--- app/page.tsx
+++ app/page.tsx
@@ -17,24 +17,24 @@ export default function Page() {
             </div>
 
             </section>
-            <h1 className="text-white text-center header mt-8 xl:mt-12 text-2xl sm:text-3xl xl:text-4xl px-4" 
+            <h1 className="text-white text-center header mt-8 xl:mt-[200px] text-2xl sm:text-3xl xl:text-6xl px-4" 
                 style={{filter: 'drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))'}}>
                 Trace and visualize like an expert
             </h1>
-            <p className="text-white text-center py-2 xl:py-4 px-4 text-sm sm:text-base">Get the lowest fees, fastest transactions, powerful APIs, and more</p>
-            <section className="max-w-screen-xl mx-auto flex flex-col xl:flex-row items-center mt-10 xl:mt-20 mb-24 xl:mb-48 px-4" >
+            <p className="text-white text-center py-2 xl:py-4 xl:text-2xl px-4 text-sm sm:text-base">Get the lowest fees, fastest transactions, powerful APIs, and more</p>
+            <section className="max-w-screen-xl mx-auto flex flex-col xl:flex-row items-center justify-between mt-10 xl:mt-20 mb-24 xl:mb-48 px-4 gap-12" >
                 <div className="flex flex-col justify-center xl:flex-1 mb-10 xl:mb-0">
-                    <div className="flex items-end mb-8">
-                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-7xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+15,000</p>
-                        <p className="text-white text-xl sm:text-2xl xl:text-2xl ml-4">daily active users.</p>
+                    <div className="flex items-center mb-12">
+                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+15,000</p>
+                        <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">daily active users.</p>
                     </div>
-                    <div className="flex items-end mb-8">
-                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-7xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+1,500</p>
-                        <p className="text-white text-xl sm:text-2xl xl:text-2xl ml-4">unique tokens tracked.</p>
+                    <div className="flex items-center mb-12">
+                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+1,500</p>
+                        <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">unique tokens tracked.</p>
                     </div>
-                    <div className="flex items-end mb-8">
-                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-7xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+3,000</p>
-                        <p className="text-white text-xl sm:text-2xl xl:text-2xl ml-4">smart contracts analyzed.</p>
+                    <div className="flex items-center mb-12">
+                        <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+3,000</p>
+                        <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">smart contracts analyzed.</p>
                     </div>
                 </div> 
                 <div className="flex flex-1 items-center justify-center">
@@ -43,12 +43,12 @@ export default function Page() {
                         loop
                         muted 
                         src="/videos/video-5.mp4"
-                        className="h-full w-full xl:h-3/4 xl:w-3/4 rounded-lg object-cover"
+                        className="h-full w-full xl:h-4/4 xl:w-4/4 rounded-lg object-cover"
                     />
                 </div>   
             </section>
-            <section className="w-full flex flex-col items-center justify-center px-4 mb-16">
-                <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6 text-center">More of your interest.</h2>
+            <section className="w-full flex flex-col items-center justify-center px-4 mb-16 xl:py-16">
+                <h2 className="text-2xl sm:text-4xl font-bold text-white mb-12 text-center">More of your interest.</h2>
                 <div className="flex flex-wrap justify-center gap-8 xl:gap-16">
                     <div className="w-full sm:w-80 md:w-96 xl:w-[500px] aspect-w-16 aspect-h-9">
                         <iframe
