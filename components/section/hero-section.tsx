4c5bec0ee0ce1fea5a75ac0e877c70f6e47baf77
Levironexe <nguyenphuoc4805@gmail.com>
2024-09-29 17:36:52 +0700

    BlockScan project

--- components/section/hero-section.tsx
+++ components/section/hero-section.tsx
@@ -1,21 +1,69 @@
 "use client";
 import React from 'react';
 import Link from 'next/link';
+import splitString from '@/app/utils/splitText';
+import  {motion} from "framer-motion";
+
+
+
+
+
+const heading1 = 'Your Trusted Scanner';
+const heading2 = ' for Fast and Safe Blockchain Transactions';
+const subtitle = 'Built all-in-one, BlockScan helps you get used to cryptocurrency.';
+const charVariants = {
+    hidden: {opacity: 0},
+    reveal: {opacity: 1},
+}
+
+
 
 const Hero = () => {
+    const splitHeading1 = splitString(heading1);
+    const splitHeading2 = splitString(heading2);
+    const splitSubtitle = splitString(subtitle);
+
+
     return (
         <div className="flex flex-col xl:flex-row max-w-screen-xl mx-auto py-8 lg:py-12 gap-8 mb-16 xl:mb-28 min-h-[600px] xl:h-[850px] items-center px-4 xl:px-0">
             
             {/* Text content */}
             <div className="flex-1 xl:flex-[2] flex items-center justify-center px-4 xl:px-8">
                 <div>
-                    <h1 className="hero__title text-primary-red text-4xl sm:text-5xl lg:text-6xl xl:text-7xl leading-relaxed xl:leading-relaxed mb-4 xl:mb-6">
-                        <span style={{filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))',}}>Your Trusted Scanner </span>
-                        <span className="text-white" style={{filter: 'drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))',}}>for Fast and Safe Blockchain Transactions</span>
-                    </h1>
-                    <p className="hero__subtitle text-white text-lg sm:text-xl lg:text-2xl mb-8 xl:mb-14">
-                        Built all-in-one, JBiz helps you get used to cryptocurrency.
-                    </p>
+                    <motion.h1 
+                    className="hero__title text-primary-red text-4xl sm:text-5xl lg:text-6xl xl:text-7xl leading-relaxed xl:leading-relaxed mb-4 xl:mb-6"
+                    initial="hidden"
+                    whileInView="reveal"
+                    transition={{staggerChildren: 0.03}}
+                    >
+                        <span style={{filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))',}}>{splitHeading1.map(char =>
+                            <motion.span key={char} transition={{duration: 0.5}} variants={charVariants}>
+                                {char}
+                            </motion.span>
+                        )}</span>
+                        <span className="text-white" style={{filter: 'drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))',}}>
+                            {splitHeading2.map(char =>
+                                <motion.span key={char} transition={{duration:0.5}} variants={charVariants}>
+                                    {char}
+                                </motion.span>
+                            )}
+                        </span>
+                    </motion.h1>
+                    <motion.p 
+                        className="hero__subtitle text-white text-lg sm:text-xl lg:text-2xl mb-8 xl:mb-14"
+                        initial='hidden'
+                        whileInView='reveal'
+                        transition={{
+                            staggerChildren: 0.01,
+                            delayChildren: 2,
+                        }}
+                    >
+                        {splitSubtitle.map( char =>
+                            <motion.span key={char} transition={{duration: 0.5}} variants={charVariants}>
+                                {char}
+                            </motion.span>
+                        )}
+                    </motion.p>
 
                    
                     <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 xl:space-x-6 mt-8 xl:mt-14">
--- components/section/reliability-section.tsx
+++ components/section/reliability-section.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React from 'react';
+import  {motion} from "framer-motion";
+import { duration } from 'html2canvas/dist/types/css/property-descriptors/duration';
 
+const fadeUp = {
+    hidden: {opacity: 0, y: 50},
+    reveal: {opacity: 1, y: 0},
+}
 const ReliabilitySection = () => {
   return (
     <main>
@@ -9,20 +15,41 @@ const ReliabilitySection = () => {
             </h1>
             <p className="text-white text-center py-2 xl:py-4 xl:text-2xl px-4 text-sm sm:text-base">Get the lowest fees, fastest transactions, powerful APIs, and more</p>
             <section className="max-w-screen-xl mx-auto flex flex-col xl:flex-row items-center justify-between mt-10 xl:mt-20 mb-24 xl:mb-28 px-4 gap-12" >
-                <div className="flex flex-col justify-center xl:flex-1 mb-10 xl:mb-0">
-                    <div className="flex items-center mb-12">
+                <motion.div 
+                className="flex flex-col justify-center xl:flex-1 mb-10 xl:mb-0"
+                initial='hidden'
+                whileInView='reveal'
+                transition={{ staggerChildren: 0.2 }}
+                >
+                    
+                    <motion.div 
+                    className="flex items-center mb-12"
+                    transition={{duration: 0.5, delay:0.2}}
+                    variants={fadeUp}>
                         <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+15,000</p>
                         <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">daily active users.</p>
-                    </div>
-                    <div className="flex items-center mb-12">
+                    </motion.div>
+
+                    <motion.div 
+                    className="flex items-center mb-12"
+                    transition={{duration: 0.5, delay:0.4}}
+                    variants={fadeUp}>
                         <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+1,500</p>
                         <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">unique tokens tracked.</p>
-                    </div>
-                    <div className="flex items-center mb-12">
+                    </motion.div>
+
+
+                    <motion.div 
+                    className="flex items-center mb-12"
+                    transition={{duration: 0.5, delay:0.6}}
+                    variants={fadeUp}>
                         <p className="font-bold text-primary-red text-5xl sm:text-6xl xl:text-8xl text-shadow-glow-red" style={{ filter: 'drop-shadow(0 0 10px rgba(231, 33, 6, 0.5))' }}>+3,000</p>
                         <p className="text-white text-xl sm:text-2xl xl:text-[27px] font-semibold ml-4">smart contracts analyzed.</p>
-                    </div>
-                </div> 
+                    </motion.div>
+
+
+
+                </motion.div> 
                 <div className="flex flex-1 items-center justify-center">
                     <video
                         autoPlay
