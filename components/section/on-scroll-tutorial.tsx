4a2252db557434b6c9ec976de823760bb3c02ab8
Levironexe <nguyenphuoc4805@gmail.com>
2024-11-05 15:49:42 +0700

    99%

--- components/section/on-scroll-tutorial.tsx
+++ components/section/on-scroll-tutorial.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { RiliabilitySection } from '..';
-import {motion} from 'framer-motion'
 
 const OnScrollSection = () => {
   const [scrollY, setScrollY] = useState(0);
